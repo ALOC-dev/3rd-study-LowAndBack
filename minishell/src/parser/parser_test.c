dfad53f330978fe90a323a7ae1108d867d330ac6
ktollonr <agetor3210@naver.com>
2025-05-19 15:42:59 +0900

    parser command 처리 방식 수정

--- minishell/src/parser/parser_test.c
+++ minishell/src/parser/parser_test.c
@@ -11,16 +11,24 @@ typedef struct ParsedCommand {
     ArgNode* arg_list;
     char* command;
     int argc;
+    struct ParsedCommand* command_list;
 } ParsedCommand;
+
+typedef struct CommandList {
+    ParsedCommand* cmd_list;
+    int command_count;
+} cmdL;
 
 void initCmd(ParsedCommand* P) {
     P -> arg_list = NULL;
     P -> command = NULL;
     P -> argc = 0;
+    P -> command_list = NULL;
 }
 
 ParsedCommand* parse_input(const char* input) {
     ParsedCommand* cmd = malloc(sizeof(ParsedCommand));
+    ArgNode* new_node = malloc(sizeof(ArgNode));
     ArgNode* last_node = NULL;
 
     initCmd(cmd);
@@ -29,21 +37,20 @@ ParsedCommand* parse_input(const char* input) {
     char* token = strtok(copy, " "); // 공백 기준으로 문자열 분리
 
     while (token) {
-        ArgNode* new_node = malloc(sizeof(ArgNode));
-
-        new_node -> arg_str = strdup(token); // token으로 분리된 단어 저장
-        new_node -> next = NULL;
-
-        if (!(cmd->arg_list)) { // 연결리스트가 비어있다면 true (첫번째 노드라면)
-            cmd -> arg_list = new_node;
+        if (!(cmd->command)) { // 연결리스트가 비어있다면 true (첫번째 노드라면)
             cmd -> command = strdup(token); // 첫번째 단어이므로 명령어에 저장
         } else {
-            last_node -> next = new_node; // 첫번째 노드가 아니라면 이전 node와 현재 노드와 연결
+            new_node -> arg_str = strdup(token); // token으로 분리된 단어 저장
+            new_node -> next = NULL;
+
+            if(last_node == NULL) {
+                last_node = new_node;
+            } else {
+                last_node -> next = new_node; // 첫번째 노드가 아니라면 이전 node와 현재 노드와 연결
+                last_node = new_node; // 다음 들어올 노드와 이어주기 위해 last_node로 참조
+            }
+            cmd -> argc++;
         }
-
-        last_node = new_node; // 다음 들어올 노드와 이어주기 위해 last_node로 참조
-        cmd -> argc++;
-
         token = strtok(NULL, " "); // NULL을 넣어주면 이미 분리된 문자열 그대로 사용함
     }
 
