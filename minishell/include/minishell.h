c1137a4211f644fbf9901d67504e714351cc4bef
박성진 <psjhappy1@gmail.com>
2025-05-26 13:12:55 +0900

    minishell.h 저장오류 수정

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -6,28 +6,8 @@
 # include <string.h>
 # include <unistd.h>
 
-typedef struct s_command {
-    char *keyword;              // 첫 토큰: 명령어
-    char **args;                // 나머지 인자들
-    int argc;                   // 인자 개수
-    char *infile;
-    char *outfile;
-    struct s_command *next;
-} t_command;
-
-// parser
-t_command *parse_input(char *input);
-void print_commands(t_command *cmd);
-void free_commands(t_command *cmd);
-
-// builtin
-int is_builtin(char *cmd);
-void run_builtin(t_command *cmd);
-
-// 각각의 함수들
-void ft_echo(t_command *cmd);
-void ft_pwd(void);
-void ft_cd(t_command *cmd);
-void ft_exit(void);
+# include "parser.h"
+# include "builtin.h"
+# include "executor.h"
 
 #endif
